a4c78bbf376c7e6193077f101d45b9c4b25cdaa3
Sadahura007 <coolden86@mail.ru>
2026-04-02 23:20:21 +1000

    Обработка нажатий клавиш WASD (и русских букв)
    Мы научились ходить

--- entities/player.py
+++ entities/player.py
@@ -4,3 +4,20 @@ class Player(Entity):
     def __init__(self, x, y, hp, damage, symbol):
         super().__init__(x, y, hp, damage, "@")
     
+    def handle_input(self, key: str, game_map) -> bool:
+        dx, dy = 0, 0
+        k = key.lower()
+
+        if k in ['w', 'ц']:      # вверх
+            dy = -1
+        elif k in ['s', 'ы']:    # вниз
+            dy = 1
+        elif k in ['a', 'ф']:    # влево
+            dx = -1
+        elif k in ['d', 'в']:    # вправо
+            dx = 1
+        else:
+            return False
+
+        # Пытаемся двигаться
+        return self.move(dx, dy, game_map)
