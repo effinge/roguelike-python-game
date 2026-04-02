fbf0f34b6a7d4d2c01b3590a86fb3811070a622f
myfreeqn <vladislav060407@gmail.com>
2026-04-02 22:23:53 +1000

    Функция для проверки возможности на прохождение сквозь стены
    false if not possible

--- map/game_map.py
+++ map/game_map.py
@@ -2,3 +2,7 @@ class GameMap:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+    
+    def is_walkable(self, x, y):
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+            return False
