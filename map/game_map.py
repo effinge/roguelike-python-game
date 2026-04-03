3e9d3db1cc43999fddd6de86d8697375e8901f14
myfreeqn <vladislav060407@gmail.com>
2026-04-03 20:14:18 +1000

    def set_floor
    делает клетку x,y проходимой меняя ее значение в массиве tiles на '.'
    возвращает None

--- map/game_map.py
+++ map/game_map.py
@@ -5,6 +5,11 @@ class GameMap:
         self.tiles = [['T' for _ in range(height)] for _ in range(width)] #двумерный список клеток
         self.objects = [['O' for _ in range(height)] for _ in range(width)] #двумерный список объектов
 
+    def set_floor(self, x, y) -> None:
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.tiles[x][y] = '.'
+
     def is_walkable(self, x, y):
-        if x < 0 or x >= self.width or y < 0 or y >= self.height:
-            return False
+        if 0 <= x < self.width and 0 <= y < self.height:
+            return self.tiles[x][y] == '.'
+        return False
