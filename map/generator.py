7f35b51bdc208b21b3b24437ae6a7ccc4ed04e4a
myfreeqn <vladislav060407@gmail.com>
2026-04-03 21:05:37 +1000

    def generate_rooms
    берет "рандомные" 2 числа w и h в диапазоне от min_size до max_size, через них считает x = рандомное в диапазоне (1, w) и y = рандомное в диапазоне (1, h).
    Если такой прямоугольник не занят - создается комната

--- map/generator.py
+++ map/generator.py
@@ -1,4 +1,5 @@
 import json
+import random
 from .game_map import GameMap
 
 class MapGenerator:
@@ -14,3 +15,22 @@ class MapGenerator:
         self.room_max_size = self.config["room_max_size"]
         self.num_enemies = self.config["num_enemies"]
         self.num_items = self.config["num_items"]
+    
+    def generate_rooms(self, game_map: GameMap) -> list:
+        rooms = []
+        max_attempts = 1000
+
+        for _ in range(self.num_rooms):
+            for _ in range(max_attempts):
+                w = random.randint(self.room_min_size, self.room_max_size)
+                h = random.randint(self.room_min_size, self.room_max_size)
+                x = random.randint(1, game_map.width - w - 1)
+                y = random.randint(1, game_map.height - h - 1)
+
+                new_rect = (x, y, w, h)
+                # Проверка пересечения с существующими комнатами
+                if not any(self._rectangles_intersect(new_rect, r) for r in rooms):
+                    rooms.append(new_rect)
+                    self._create_room(game_map, x, y, w, h)
+                    break
+        return rooms
