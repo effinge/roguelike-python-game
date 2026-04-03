44e552dbacf13901c9bed7fe2b1b065b41444fdf
myfreeqn <vladislav060407@gmail.com>
2026-04-03 19:42:15 +1000

    init map
    создает пустую карту размера width*height. есть поля свободных клеток tiles и объектов objects в виде двумерных списков

--- map/game_map.py
+++ map/game_map.py
@@ -2,7 +2,9 @@ class GameMap:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-    
+        self.tiles = [['T' for _ in range(height)] for _ in range(width)] #двумерный список клеток
+        self.objects = [['O' for _ in range(height)] for _ in range(width)] #двумерный список объектов
+
     def is_walkable(self, x, y):
         if x < 0 or x >= self.width or y < 0 or y >= self.height:
             return False
