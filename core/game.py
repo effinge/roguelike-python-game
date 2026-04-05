e69e346da3c3e9fe6868253dcedac79101b3a7c3
effinge <116564417+effinge@users.noreply.github.com>
2026-04-05 20:17:26 +1000

    ф-ия нахождения обьекта + новые данные от конфига

--- core/game.py
+++ core/game.py
@@ -1,28 +1,29 @@
 import json
 
-from map.game_map import GameMap
+from map.generator import MapGenerator
 from entities.player import Player
+from ui.renderer import Renderer
 
 class Game:
     def __init__(self):
         self.config = self.load_config()
         
-        map_width = self.config["map"]["width"]
-        map_height = self.config["map"]["height"]
-        
-        player_x = self.config["player"]["start_x"]
-        player_y = self.config["player"]["start_y"]
-        player_hp = self.config["player"]["hp"]
-        player_damage = self.config["player"]["damage"]
-        
-        self.game_map = GameMap(map_width,map_height)
-        self.player = Player(player_x,player_y,player_hp,player_damage)
-        
+        self.game_map = None
+        self.player = None
+        self.renderer = Renderer()
         self.is_running = True
+        
+        self.setup_game()
         
     def load_config(self):
         with open("config/game_config.json", "r") as file:
             return json.load(file)
-        
+    
+    def find_object(self, symbol):
+        for x in range(self.game_map.width):
+            for y in range(self.game_map.height):
+                if self.game_map.objects[x][y] == symbol:
+                    return (x,y)
+                
     def run(self):
         print("test load")
