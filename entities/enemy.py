d46af084f93fc92d8af9d11c238bd62f7ad07721
effinge <116564417+effinge@users.noreply.github.com>
2026-04-08 11:09:38 +1000

    pull (#9)
    * добавлен класс гоблина
    
    теперь + мобик
    
    * Ход врага добавлен
    
    Атакует зараза
    
    * Случайный движение и  атаки
    
    оно живое
    
    * """Перемещает существо, если клетка проходимая"""
    
    ---
    
    * Игрок атакует цель, если она рядом
    
    ---
    
    * класс исправил
    
    * удаление с карты мертового
    
    Вызывается, когда существо умирает
    
    * исправил порядок
    
    -
    
    * кдаление с карты
    
    .
    
    * исправил удаление с карты существа
    
    -
    
    * удаление врага с карты Левчик инициируй
    
    .
    
    * удаление с карты бахнул
    
    * убрано ненужное
    
    123
    
    * дипсик классик
    
    * -ненужное
    
    123
    
    ---------
    
    Co-authored-by: Sadahura007 <coolden86@mail.ru>

---
+++ entities/enemy.py
@@ -0,0 +1,25 @@
+import random
+from .entity import Entity
+
+class Enemy(Entity):
+
+    def __init__(self, x, y, hp, damage, symbol):
+        super().__init__(x, y, hp, damage, symbol)
+
+    def ai_move(self, game_map, player):
+        if not self.is_alive():
+            return
+
+        if abs(self.x - player.x) <= 1 and abs(self.y - player.y) <= 1:
+            damage = self.attack(player)
+            return
+
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        dx, dy = random.choice(directions)
+        self.move(dx, dy, game_map)
+
+    def die(self, game_map=None):
+        print(f"{self.name} погиб!")
+
+        if game_map:
+            self.remove_from_map(game_map)
